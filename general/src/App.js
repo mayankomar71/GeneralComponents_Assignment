156067890e40e64cec6ee3e5a00cd7117efaacab
Mayank Omar <mayankomar71@gmail.com>
2019-04-27 17:10:06 +0530

    Assignment General Components

--- general/src/App.js
+++ general/src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import './App.css';
-// import FormComponent from './components/formcomponent'
-// import Input from './components/inputcomponent'
 import Signuplogin from './components/signuplogin'
 
 function App() {
